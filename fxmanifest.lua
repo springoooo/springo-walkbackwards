f0f03660c018370a64a08af651f4f24754abd2ce
Springo_dev <mystorp2@gmail.com>
2024-09-08 19:05:57 +1000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- https://github.com/springoooo
+author 'Springo'
+title 'springo-walkbackwards'
+description 'Allows you to walk backwards while in third person.'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+lua54 'yes'
